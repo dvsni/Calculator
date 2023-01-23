daad3708eefcd3d66c1462252ea5f1640512eedb
dvsni <danil.shir@outlook.com>
2023-01-23 18:37:55 +0300

    controller

--- controller.py
+++ controller.py
@@ -4,17 +4,17 @@ import logger as log
 import parser_exp as pars
 
 def run():
-    value_type = ui.input_value_type()
-    mode_work = ui.input_mode()
+    value_type = user_interface.input_value_type()
+    mode_work = user_interface.input_mode()
     log.log_type(value_type)
     log.log_mode(mode_work)
     exp_string=''
     if mode_work=='2':
-        exp_string=ui.input_digits()
+        exp_string=user_interface.input_digits()
     if mode_work=='1':
-        exp_string=ui.expression()
+        exp_string=user_interface.expression()
     expression=pars.exp_to_list(exp_string)
     log.log_exp(exp_string)
     result=calc.calculate(expression,value_type)
     log.log_result(result)
-    ui.print_result(exp_string,result[0])
+    user_interface.print_result(exp_string,result[0])
